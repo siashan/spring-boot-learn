846209445aaa6d4c02aa9bb86943b22b5b5bbaaa
siashan <771595980@qq.com>
2020-03-08 17:26:27 +0800

    banner展示

--- src/main/java/com/siashan/springboot/SpringbootApplication.java
+++ src/main/java/com/siashan/springboot/SpringbootApplication.java
@@ -1,5 +1,6 @@
 package com.siashan.springboot;
 
+import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,7 +8,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringbootApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringbootApplication.class, args);
+        //SpringApplication.run(SpringbootApplication.class, args);
+
+        SpringApplication app = new SpringApplication(SpringbootApplication.class);
+        app.setBannerMode(Banner.Mode.OFF);
+        app.run(args);
     }
 
 }
